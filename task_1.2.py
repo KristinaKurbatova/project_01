68bfdfe4d56b1e7bfeb5c0da18694c6fe16acbcc
KristinaKurbatova <79222812137@ya.ru>
2023-04-28 00:20:13 +0500

    Solution for point B

--- task_1.2.py
+++ task_1.2.py
@@ -18,12 +18,12 @@ my_favorite_songs = [
     ['In This World', 4.02],
     ]
 
-def main():
-  import random
-  songs = random.sample(my_favorite_songs, 3)
-  print(songs)
-  total_time = sum([l for  n, l in songs])
-  print('Три песни звучат', round(total_time, 2), 'минут')
+import random
+songs = random.sample(my_favorite_songs, 3)
+
+total_time = sum([l for  n, l in songs])
+
+print('Три песни звучат', round(total_time, 2), 'минут')
 
 
 
@@ -32,10 +32,29 @@ def main():
 # Распечатайте общее время звучания трех случайных песен
 # Вывод: Три песни звучат ХХХ минут.
 
-#my_favorite_songs_dict = {
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
 
+songs_lst = list(my_favorite_songs_dict.items())
 
+import random
+three_songs = random.sample(songs_lst, 3)
+  
+total_time = 0
 
+for i in three_songs:
+    total_time += i[1]
+    
+print(f'Три песни звучат  {round(total_time, 2)} минут')
 
 # Дополнительно для пунктов A и B
 # Пункт C.
